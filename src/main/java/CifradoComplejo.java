fa8a3682422d07fdcc4c262ee8dec3262805f5a6
mihai <mihaitagidu@gmail.com>
2026-02-04 16:49:19 +0100

    commit inicial

---
+++ src/main/java/CifradoComplejo.java
@@ -0,0 +1,14 @@
+public class CifradoComplejo {
+
+    private static final String ALFABETO = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    public static String procesarMensaje(String texto, int clave, boolean esCifrado) {
+        StringBuilder resultado = new StringBuilder();
+
+        for (int i=0; i<texto.length(); i++){
+            char caracterActual = texto.charAt(i);
+
+
+        }
+    }
+}
